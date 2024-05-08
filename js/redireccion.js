f668d96c05a132ed01a0b83c9c88e276ea067a48
Disenandowebs <41397439+Disenandowebs@users.noreply.github.com>
2024-05-08 18:37:20 -0400

    Sitio actualizado

--- js/redireccion.js
+++ js/redireccion.js
@@ -1,5 +1,3 @@
-// redireccion.js
-// Espera 5 segundos (5000 milisegundos) y luego redirige
 setTimeout(function() {
-    window.location.href = 'https://resume-359e8.web.app'; // Cambia la URL a la que deseas redirigir
+    window.location.href = 'https://resume-359e8.web.app';
 }, 100);
